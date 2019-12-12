7f1482e9c3f3ede7350a592f92ebc414bf9c6440
Divyanshu Sachdeva <ds997@njit.edu>
2019-12-12 10:28:19 -0500

    Feature: RestAPI created for populating Cat object into the database.

---
+++ src/app.py
@@ -0,0 +1,19 @@
+import json
+
+from flask import request
+
+from . import create_app, database
+from .pet_model import Cats
+
+app = create_app()
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    name = data['name']
+    price = data['price']
+    breed = data['breed']
+
+    database.add_instance(Cats, name=name, price=price, breed=breed)
+    return json.dumps("Added"), 200
---
+++ src/database.py
@@ -0,0 +1,11 @@
+from .pet_model import db
+
+
+def add_instance(model, **kwargs):
+    instance = model(**kwargs)
+    db.session.add(instance)
+    commit_changes()
+
+
+def commit_changes():
+    db.session.commit()
